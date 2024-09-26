21ddcad5aa234a8dfbd07d552571d4782c97a722
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2024-09-26 17:13:27 +0100

    Updated my Ceaser encoding and decoding program

--- caesa_cipher.py
+++ caesa_cipher.py
@@ -7,37 +7,17 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
+def ceaser(start_text, shift_amount, ceaser_direction):
+    end_text = ""
+    if ceaser_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
+    print(f"Here is the {ceaser_direction}d result: {end_text}")
+
+ceaser( start_text= text,shift_amount= shift, ceaser_direction= direction)
 
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-    #e.g.
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        plain_text +=alphabet[new_position]
-    print(f"The decoded text is {plain_text}")
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-if direction == "encode":
-    encrypt(plain_text= text, shift_amount= shift)
-elif direction == "decode":
-    decrypt(cipher_text= text, shift_amount= shift)
+    #
